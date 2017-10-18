0924034b60f02a80ea8a9522bc1ced0186185f00
AnthonyYang8017 <g021876@gmail.com>
2017-10-18 18:23:57 +1100

    Add files via upload

--- COMP3421assign2/src/ass2/Terrain.java
+++ COMP3421assign2/src/ass2/Terrain.java
@@ -16,14 +16,15 @@ import ass2.MathUtil;
  *
  * @author malcolmr
  */
-public class Terrain extends GameObject {
+public class Terrain {
 
     private Dimension mySize;
-    private static double[][] myAltitude;
+    private double[][] myAltitude;
     private List<Tree> myTrees;
     private List<Road> myRoads;
-    private Avatar myAvatar;
     private float[] mySunlight;
+    
+    private MyTexture groundTexture;
 
     /**
      * Create a new terrain
@@ -31,11 +32,7 @@ public class Terrain extends GameObject {
      * @param width The number of vertices in the x-direction
      * @param depth The number of vertices in the z-direction
      */
-    public Terrain(GameObject parent) {
-		super(parent);
-	}
     public Terrain(int width, int depth) {
-    	this(GameObject.ROOT);
         mySize = new Dimension(width, depth);
         myAltitude = new double[width][depth];
         myTrees = new ArrayList<Tree>();
@@ -43,10 +40,15 @@ public class Terrain extends GameObject {
         mySunlight = new float[3];
     }
     
+    
     public Terrain(Dimension size) {
         this(size.width, size.height);
     }
 
+    public void setGroundTexture (MyTexture texture){
+    	groundTexture = texture;
+    }
+    
     public Dimension getsize() {
         return mySize;
     }
@@ -70,7 +72,7 @@ public class Terrain extends GameObject {
      * @param z
      * @return
      */
-    public static double getGridAltitude(int x, int z) {
+    public double getGridAltitude(int x, int z) {
         return myAltitude[x][z];
     }
     
@@ -128,27 +130,10 @@ public class Terrain extends GameObject {
      * @param z
      * @return
      */
-    public static double altitude(double x, double z) {
+    public double altitude(double x, double z) {
         double altitude = 0;
-        double i = (double)(int) x;
-        double j = (double)(int) z;
-        double heightXZ = getGridAltitude((int)i,(int)j);
-        double heightX1Z = getGridAltitude((int)i+1,(int)j);
-        double heightXZ1 = getGridAltitude((int)i,(int)j+1);
-        double heightX1Z1 = getGridAltitude((int)i+1,(int)j+1);
-        double temi = x-i;
-        double temj = z-j;
-        if (temi + temj <= 1){
-        	altitude = (heightXZ + (heightX1Z - heightXZ)*temi)
-        			+((heightXZ1 + (heightX1Z - heightXZ1)*temi)
-        			-(heightXZ + (heightX1Z - heightXZ)*temi))
-        			*(temj/Math.sqrt(2));
-        }else{
-        	altitude =(heightXZ1 + (heightX1Z - heightXZ1)*temi)
-        			+((heightXZ1 + (heightX1Z1 - heightXZ1)*temi)
-        			-(heightXZ1 + (heightX1Z - heightXZ1)*temi))
-        			*(temj/Math.sqrt(2));
-        }
+
+        
         
         return altitude;
     }
@@ -161,8 +146,9 @@ public class Terrain extends GameObject {
      * @param z
      */
     public void addTree(double x, double z) {
-        double y = altitude(x, z);
-        Tree tree = new Tree(this);
+        //double y = altitude(x, z);
+    	double y = getGridAltitude((int)x, (int)z);
+        Tree tree = new Tree();
         tree.setPosition(x, y, z);
         myTrees.add(tree);
     }
@@ -178,80 +164,105 @@ public class Terrain extends GameObject {
         Road road = new Road(width, spine);
         myRoads.add(road);        
     }
-    
-    public void addAvatar() {
-    	myAvatar = new Avatar(this);
-    	myAvatar.setPosition(0,altitude(0,0),0);
-    }
 
-	public void drawSelf(GL2 gl) {
+	public void drawTerrain(GL2 gl) {
 		
 		gl.glPushMatrix();
-		
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl.glTranslated(-10, 0, -10);
 		gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
 		//gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
-		//gl.glColor4d(0, 0, 0, 0);
+		//gl.glColor4d(0, 1, 0, 0);
 		gl.glLineWidth(10);
 		
-		float[] ambient = {0.1f, 0.5f, 0.1f, 1.0f};
-	    float[] diffuse = {0.1f, 0.5f, 0.1f, 1.0f};
+		float[] ambient = {0.1f, 0.3f, 0.1f, 1.0f};
+	    float[] diffuse = {0.1f, 0.3f, 0.1f, 1.0f};
 	    float[] specular = {0.1f, 0.1f, 0.1f, 1.0f};
 	  
 	    gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_AMBIENT, ambient, 0);
 	    gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_DIFFUSE, diffuse, 0);
 	    gl.glMaterialfv(GL2.GL_FRONT, GL2.GL_SPECULAR, specular, 0);
-	 
+	    
+	    
+        gl.glTexEnvf(GL2.GL_TEXTURE_ENV, GL2.GL_TEXTURE_ENV_MODE, GL2.GL_MODULATE);
+        gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_WRAP_S, GL2.GL_REPEAT); 
+        gl.glTexParameteri(GL2.GL_TEXTURE_2D, GL2.GL_TEXTURE_WRAP_T, GL2.GL_REPEAT); 
+        gl.glBindTexture(GL2.GL_TEXTURE_2D, groundTexture.getTextureId());
+        
+		
 	    for (int z = 0; z < mySize.height -1; z++) {
 	        for (int x = 0; x < mySize.width -1; x++) {
 	        	double[] v;
+	        	//double[] v1 = {x, getGridAltitude(x, z), z};
+	            //double[] v2 = {x, getGridAltitude(x, z + 1), z + 1};
+	            //double[] v3 = {x + 1, getGridAltitude(x + 1, z), z};
+	            //double[] v4 = {x + 1, getGridAltitude(x + 1, z + 1), z + 1};
+	            //v = MathUtil.getNormal(v1, v2, v3);
+	            //gl.glNormal3dv(v, 0);
+	        	
+	        	
+	        	 
+	        	
 	            gl.glBegin(GL2.GL_TRIANGLES);
 	            {
+	            	
+	              //gl.glColor3f(0.0f, 1.0f, 0.0f); //Green colour (does nothing if lighting enabled)
 	              
 	              v = NormalProcesser(x, getGridAltitude(x, z), z);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(0, 0);
 	              gl.glVertex3d(x, getGridAltitude(x, z), z);
 	              
 	              v = NormalProcesser(x, getGridAltitude(x, z + 1), z + 1);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(1, 0.5);
 	              gl.glVertex3d(x, getGridAltitude(x, z + 1), z + 1);
 	              
 	              v = NormalProcesser(x + 1, getGridAltitude(x + 1, z), z);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(0, 1);
 	              gl.glVertex3d(x + 1, getGridAltitude(x + 1, z), z);
 	            }
 	            gl.glEnd();
 	            
+	            //double[] v5 = MathUtil.getNormal(v3, v2, v4);
+	            //gl.glNormal3dv(v5, 0);
+
+	               
+	              
 	            gl.glBegin(GL2.GL_TRIANGLES);
 	            {
+	              //gl.glColor3f(0.0f, 1.0f, 0.0f); //Green colour (does nothing if lighting enabled)
+	              
+	 
 	              
 	              v = NormalProcesser(x + 1, getGridAltitude(x + 1, z), z);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(0, 0);
 	              gl.glVertex3d(x + 1, getGridAltitude(x + 1, z), z);
+	             
 	              
 	              v = NormalProcesser(x, getGridAltitude(x, z + 1), z + 1);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(1, 0.5);
 	              gl.glVertex3d(x, getGridAltitude(x, z + 1), z + 1);
 	              
 	              v = NormalProcesser(x + 1, getGridAltitude(x + 1, z + 1), z + 1);
 	              gl.glNormal3d(v[0],v[1],v[2]);
+	              gl.glTexCoord2d(0, 1);
 	              gl.glVertex3d(x + 1, getGridAltitude(x + 1, z + 1), z + 1);
+	              
 	            }
 	            gl.glEnd();
 	        }
 	    }
+	    //draw tree
+	    GameObject.ROOT.draw(gl);
 	    
 	    gl.glPopMatrix();
 	    gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
 	    
-	    myAvatar.setPosition(10-Game.positionX, altitude(10-Game.positionX,10-Game.positionZ), 10-Game.positionZ);
-	    
 	}
-	
-	public void update(double dt) {
-        // do nothing
-		//myAvatar.setPosition(Game.positionX-10, 0, Game.positionZ-10);
-		
-    }
 	
 	public double[] NormalProcesser(int x, double y, int z) {
 		double[] normal;
